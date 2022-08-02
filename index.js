e6871ca148997d4b9fed171215f395cd4decf77b
Your name <wong.shiuy@gmail.com>
2022-08-02 00:27:14 -0400

    commit tree 9333f5be0ad6c7d0a288017ce291d0979f11524e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {    
+        return Math.abs(distance - 42)
+    }
+
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264;
+     // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
